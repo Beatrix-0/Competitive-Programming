b1e564d9c8758de1b1957d8ea710c277986b6d0d
Md. Siam <153378754+Beatrix-0@users.noreply.github.com>
2024-11-26 13:15:19 +0600

    New

---
+++ CP-Resources/Resources.cpp
@@ -0,0 +1,8 @@
+1. https://blog.shahjalalshohag.com/topic-list/ ( *** )
+
+2. https://drive.google.com/file/d/1J2x8pIYQ3MXANgvzOgBciWd3d79j_Exa/view
+3. Prefix Sum Techneque : https://youtu.be/nZe7P674xZo?si=k3-Y56-DhADnzH2M
+4. Diffrence array Techneque : https://youtu.be/96RG7EBF8LI?si=P17XIyN_XGEWPqwM 
+5. Maximum Sum Rectangle : https://youtu.be/mKluyW4YFvs?si=Z05zDduS521Xc6W- 
+      - https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=44
+6.       
