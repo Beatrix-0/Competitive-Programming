85b75aa0ed80bb34b3fa61e1dffad149dde1e650
Md. Siam <153378754+Beatrix-0@users.noreply.github.com>
2025-10-21 10:30:19 +0600

    code

--- Let's Recape/Number Theory/Modular Arithmetic/05. Binomal co-efficient.cpp
+++ Let's Recape/Number Theory/Modular Arithmetic/05. Binomal co-efficient.cpp
@@ -2,27 +2,26 @@
 //Code :
 #include<bits/stdc++.h> 
 using namespace std;
-
+ 
 #define LL long long int
 const int mod = 1e9+7;
-
-int fact(int x)
+const int mx = 1e6;
+int f[mx];
+ 
+void pre_fact()
 {
-    int ans = 1;
-    for(int i = 2; i <= x; i++)
-    {
-        ans = (ans * 1LL * i) % mod;
+    f[0] = 1 ;
+    for ( int i = 1 ; i <= mx ; i++ ){
+        f[i] = (f[i - 1] * 1LL * i) % mod;
     }
-    return ans ;
 }
-
+ 
 int inverse ( int a , int n)
 {
     int ans = 1 ;
     while ( n )
     {
-        if ( n & 1)
-        {
+        if ( n & 1){
             ans = ( ans * 1LL * a ) % mod ;
         }
         a = ( a *1LL * a )% mod ;
@@ -30,24 +29,23 @@ int inverse ( int a , int n)
     }
     return ans ;
 }
-
+ 
 signed main()
 {
-    int n, r, a, b, c, ans, opr1, opr2;
-    cin >> n >> r;      
-    a = fact(n);
-    b = fact(r);
-    c = fact(n - r);
-    ans =  a;
-    opr1 = inverse(b, mod - 2);
-    opr2 = inverse(c, mod - 2);
-    ans = (ans * 1LL * opr1) % mod;
-    ans = (ans * 1LL * opr2) % mod;
-    cout << "nCr : " << ans << endl;
-
+    pre_fact();
+    int t ; cin >> t ;
+    while ( t-- ){
+        int n, r, a, b, c, ans, opr1, opr2;
+        cin >> n >> r;      
+        a = f[n];
+        b = f[r];
+        c = f[n-r];
+        ans =  a;
+        opr1 = inverse(b, mod - 2);
+        opr2 = inverse(c, mod - 2);
+        ans = (ans * 1LL * opr1) % mod;
+        ans = (ans * 1LL * opr2) % mod;
+        cout << ans << '\n';
+    }
     return 0;
 }
-
-
-Problem :
-1. https://codeforces.com/contest/1922/problem/B
