1550a02db35490fb89f03b42bd4518f45942c763
Md. Siam <153378754+Beatrix-0@users.noreply.github.com>
2025-10-29 23:00:38 +0600

    code

--- Let's Recape/Number Theory/Prime Number/Primality Check/01 .Primality Check(Trial Division).cpp
+++ Let's Recape/Number Theory/Prime Number/Primality Check/01 .Primality Check(Trial Division).cpp
@@ -1,5 +1,4 @@
 // Topic : Primality Check 
-// Code :
 #include<bits/stdc++.h>
 using namespace std ;
 
@@ -9,31 +8,24 @@ int main()
     cin>>n ;
 
     bool isPrime = true ;
-
-    if ( n == 0 || n == 1 )
-    {
+    if ( n == 0 || n == 1 ){
         isPrime = false ;
     }
 
-    for ( int i = 2 ; i * i <= n ; i++ )
-    {
-        if ( n % i == 0 )
-        {
+    for ( int i = 2 ; i * i <= n ; i++ ){
+        if ( n % i == 0 ){
             isPrime = false ;
             break ;
         }
     }
 
-    if ( isPrime )
-    {
+    if ( isPrime ){
         cout << "YES" << '\n';
     }
-    else
-    {
+    else{
         cout << "NO" << '\n';
     }
-
     return 0;
 }
 
-1. https://codeforces.com/problemset/problem/230/B
+// https://codeforces.com/problemset/problem/230/B
