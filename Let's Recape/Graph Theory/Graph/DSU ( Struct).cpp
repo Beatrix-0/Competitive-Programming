36b7677525e7e02f7e60c8b87c5501a0c1c7d91d
Md. Siam <153378754+Beatrix-0@users.noreply.github.com>
2025-12-11 17:25:36 +0600

    code

--- Let's Recape/Graph Theory/Graph/DSU ( Struct).cpp
+++ Let's Recape/Graph Theory/Graph/DSU ( Struct).cpp
@@ -2,42 +2,30 @@
 using namespace std ;
 
 const int mx = 2e5+123;
-struct Disjoint_set_union
-{
+struct Disjoint_set_union{
     int Parent[mx], Size[mx];
-
-    void Init(int n)
-    {
-        for (int i = 0; i <= n; i++)
-        {
+    void Init(int n){
+        for (int i = 0; i <= n; i++){
             Parent[i] = i;
             Size[i] = 1;
         }
     }
-
-    int Find(int v)
-    {
+    int Find(int v){
         if (v == Parent[v]) return v;
-        // Path compression
         return Parent[v] = Find(Parent[v]);
     }
-
-    bool Union(int u, int v)
-    {
+    bool Union(int u, int v){
         int root_u = Find(u);
         int root_v = Find(v);
-
-        if (root_u != root_v)
-        {
-            if (Size[root_u] < Size[root_v])
-            {
+        if (root_u != root_v){
+            if (Size[root_u] < Size[root_v]){
                 swap(root_u, root_v);
             }
             Parent[root_v] = root_u;
             Size[root_u] += Size[root_v];
-            return true;                  // Union performed
+            return true; 
         }
-        return false; // 'u' and 'v' are already in the same set
+        return false;
     }
 } DSU;
 
@@ -49,27 +37,19 @@ int main()
     int n , m ;
     cin>> n >> m ;
     DSU.Init(n);
-    while ( m-- )
-    {
+    while ( m-- ){
         int u , v ;
         cin>> u >> v ;
         DSU.Union( u , v );
     }
 
-    int connected_component = 0 ;
-    for ( int i = 1 ; i <= n ; i++ )
-    {
-        if ( DSU.Find(i) == i )
-        {
-            connected_component++;
+    int conCmp = 0 ;
+    for ( int i = 1 ; i <= n ; i++ ){
+        if ( DSU.Find(i) == i ){
+            conCmp++;
         }
     }
-    cout << connected_component << '\n';
-
-
+    cout << conCmp << '\n';
     return 0 ;
 }
 
-
-// Video : https://youtu.be/zEAmQqOpfzM?si=gL3xZUegJmZVxDlI 
-Problem : https://www.hackerearth.com/practice/data-structures/disjoint-data-strutures/basics-of-disjoint-data-structures/practice-problems/algorithm/city-and-flood-1/ 
