42a0a9219ded39e5a46f471da7c43b9748fd438a
Md. Siam <153378754+Beatrix-0@users.noreply.github.com>
2026-04-09 21:22:30 +0600

    code

--- Let's Recape/Data structure/Segment Tree/01.Segment tree.cpp
+++ Let's Recape/Data structure/Segment Tree/01.Segment tree.cpp
@@ -4,20 +4,17 @@ using namespace std;
 const int N = 3e5 + 9;
 int a[N];
 
-struct ST
-{
+struct ST{
     int t[4 * N];
     static const int inf = 1e9;
 
-    ST()
-    {
+    ST(){
         memset(t, 0, sizeof t);
     }
 
     void build(int n, int b, int e)
     {
-        if (b == e)
-        {
+        if (b == e){
             t[n] = a[b];
             return;
         }
@@ -29,11 +26,9 @@ struct ST
         t[n] = max(t[l], t[r]); // Change this
     }
 
-    void upd(int n, int b, int e, int i, int x)
-    {
+    void upd(int n, int b, int e, int i, int x){
         if (i < b || i > e) return;
-        if (b == e && b == i)
-        {
+        if (b == e && b == i){
             t[n] = x; // Update section
             return;
         }
@@ -45,8 +40,7 @@ struct ST
         t[n] = max(t[l], t[r]); // Change this
     }
 
-    int query(int n, int b, int e, int i, int j)
-    {
+    int query(int n, int b, int e, int i, int j){
         if (j < b || i > e) return -inf;  // return appropriate value
         if (i <= b && e <= j)
         {
@@ -59,14 +53,12 @@ struct ST
     }
 } t;
 
-int32_t main()
-{
+int32_t main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
     int n = 5;
-    for (int i = 1; i <= n; i++)
-    {
+    for (int i = 1; i <= n; i++){
         a[i] = i;
     }
 
